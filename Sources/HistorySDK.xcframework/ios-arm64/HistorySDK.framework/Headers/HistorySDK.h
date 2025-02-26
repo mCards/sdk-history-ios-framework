f272bd8bb29858fff1ad80b0a0a7b851f1e7811e
Yaro <ydukal@mcards.com>
2025-02-26 12:05:10 -0600

    - signed and published

---
+++ Sources/HistorySDK.xcframework/ios-arm64/HistorySDK.framework/Headers/HistorySDK.h
@@ -0,0 +1,18 @@
+//
+//  HistorySDK.h
+//  HistorySDK
+//
+//  Created by Mukhtar Yusuf on 12/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HistorySDK.
+FOUNDATION_EXPORT double HistorySDKVersionNumber;
+
+//! Project version string for HistorySDK.
+FOUNDATION_EXPORT const unsigned char HistorySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HistorySDK/PublicHeader.h>
+
+
